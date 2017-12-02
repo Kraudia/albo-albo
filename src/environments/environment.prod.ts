24440ce41955148e55d0bea65d8ae5f72ea69ab1
Kraudia <klaudienne@gmail.com>
2017-12-02 01:15:15 +0100

    move to a new domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URL: 'https://albo-albo.herokuapp.com/',
-  APP_URL: 'https://kraudia.github.io/albo-albo/'
+  APP_URL: 'https://albo-albo.tk/'
 };
